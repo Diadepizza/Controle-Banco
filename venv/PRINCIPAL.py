7d27aabbb3efefa77539c782274dda745d74c6a8
Diadepizza <leozinhobahr@gmail.com>
2026-03-09 16:50:13 -0300

    Terceiro Commit

--- venv/PRINCIPAL.py
+++ venv/PRINCIPAL.py
@@ -1,6 +1,11 @@
 from db import conectar
 import tkinter as tk
 
+def porhora():
+    print("Working")
+def pordia():
+    print("Working too!")
+
 janela = tk.Tk()
 janela.title("CONTROLE PRODUÇÃO")
 janela.geometry("1115x900")
@@ -8,8 +13,10 @@ janela.configure(bg="#361C29")
 conexao = conectar()
 cursor = conexao.cursor()
 
-tk.Button(text="Produção por Hora",font=("Comic Sans MS", 24),bg="#170C22",fg="white",width=28).grid(padx=256,pady=400)
-
+botao1 = tk.Button(text="Produção por Hora",font=("Comic Sans MS", 24),bg="#170C22",fg="white",width=28,command=porhora)
+botao1.grid(row=5, column=0, padx=256, pady=(430,0))
+botao2 = tk.Button(text="Produção por Dia",font=("Comic Sans MS", 24),bg="#170C22",fg="white",width=28,command=pordia)
+botao2.grid(row=6, column=0, padx=256, pady=(10,0))
 
 
 
