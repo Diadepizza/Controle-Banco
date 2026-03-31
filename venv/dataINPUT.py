45f2117895f838438bc9e66f6b35dc0eb1db3df1
Diadepizza <leozinhobahr@gmail.com>
2026-03-31 17:09:42 -0300

    Vigéssimo Commit

---
+++ venv/dataINPUT.py
@@ -0,0 +1,22 @@
+import serial
+from db import conectar
+import time
+
+arduino = serial.Serial('COM3', 9600)
+conexao = conectar()
+cursor = conexao.cursor()
+
+ultimo_tempo = time.time()
+
+while True:
+    dado = arduino.readline().decode().strip()
+
+    if dado == "REGISTRADO":
+        agora = time.time()
+        intervalo = agora - ultimo_tempo
+        ultimo_tempo = agora
+
+        print("Intervalo:", intervalo)
+
+        cursor.execute(f"INSERT INTO producao(intervalo) VALUES ({intervalo})")
+        conexao.commit()
